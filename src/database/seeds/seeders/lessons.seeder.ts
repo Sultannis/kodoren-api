744a1d28bf68a801ebf49f8f8364c81e58f5f23e
Sultan Mustafin <sultan.fb29@gmail.com>
2023-05-30 12:20:58 +0300

    refactor: update lessons seeder file data

--- src/database/seeds/seeders/lessons.seeder.ts
+++ src/database/seeds/seeders/lessons.seeder.ts
@@ -6,32 +6,31 @@ const LESSONS = [
     title: 'İlk bug düzeltme',
     courseId: 1,
     free: true,
-    codeContent: `
-      &lt;!DOCTYPE html&gt;
-        &lt;html lang=&quot;en&quot;&gt;
-          &lt;head&gt;
-            &lt;meta charset=&quot;UTF-8&quot; /&gt;
-            &lt;meta http-equiv=&quot;X-UA-Compatible&quot; content=&quot;IE=edge&quot; /&gt;
-            &lt;meta name=&quot;viewport&quot; content=&quot;width=device-width, initial-scale=1.0&quot; /&gt;
-            &lt;link
-              href=&quot;https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&amp;display=swap&quot;
-              rel=&quot;stylesheet&quot;
-            /&gt;
-            &lt;!-- &lt;link rel=&quot;stylesheet&quot; href=&quot;https://kodoren-lessons-assets.fra1.cdn.digitaloceanspaces.com/course-1-lesson-1.css&quot; /&gt; -->
-            &lt;title&gt;Kodoren store&lt;/title&gt;
-          &lt;/head&gt;
-          &lt;body&gt;
-            &lt;main class=&quot;main&quot;&gt;
-              &lt;div class=&quot;main__card&quot;&gt;
-                &lt;img src=&quot;https://kodoren-lessons-assets.fra1.cdn.digitaloceanspaces.com/kodoren-tee.jpg&quot; alt=&quot;&quot; class=&quot;main__image&quot; /&gt;
-                &lt;h1 class=&quot;main__heading&quot;&gt;Kodoren t-shirt&lt;/h1&gt;
-                &lt;p class=&quot;main__text&quot;&gt;Darkblue baggy-fit t-shirt with kodoren logo. Unisex, 100% cotton&lt;/p&gt;
-                &lt;button class=&quot;main__button&quot;&gt;Buy&lt;/button&gt;
-              &lt;/div&gt;
-            &lt;/main&gt;
-          &lt;/body&gt;
-        &lt;/html&gt;
-      `,
+    codeContent: `<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
+                rel="stylesheet"
+            />
+            <!-- <link rel="stylesheet" href="https://kodoren-lessons-assets.fra1.cdn.digitaloceanspaces.com/course-1-lesson-1.css" /> -->
+            <title>Kodoren store</title>
+        </head>
+        <body>
+            <main class="main">
+                <div class="main__card">
+                    <img src="https://kodoren-lessons-assets.fra1.cdn.digitaloceanspaces.com/kodoren-tee.jpg" alt="" class="main__image" />
+                    <h1 class="main__heading">Kodoren t-shirt</h1>
+                    <p class="main__text">Darkblue baggy-fit t-shirt with kodoren logo. Unisex, 100% cotton</p>
+                    <button class="main__button">Buy</button>
+                </div>
+            </main>
+        </body>
+    </html>
+    `,
     codeFileExtension: 'html',
   },
 ];
