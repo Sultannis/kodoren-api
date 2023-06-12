7e84a365220d7f2abf0e36ec5a067128f25915ef
Sultan Mustafin <sultan.fb29@gmail.com>
2023-06-13 01:15:36 +0300

    refactor: remove guard from health check endpoint

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,9 +1,7 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { JwtGuard } from '../auth/guards/jwt-guard';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('health')
 export class HealthController {
-  @UseGuards(JwtGuard)
   @Get()
   healthCheck(): string {
     return 'OK';
